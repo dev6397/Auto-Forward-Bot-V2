ba9bd6721a238045eab5292aacaf653fef0b758f
dev pandey <100516809+dev6397@users.noreply.github.com>
2024-09-06 09:22:45 +0530

    config.py

--- config.py
+++ config.py
@@ -1,9 +1,9 @@
 from os import environ 
 
 class Config:
-    API_ID = environ.get("API_ID", "577678")
-    API_HASH = environ.get("API_HASH", "d2c6e01uuiuiouioiuiou0fc6d7a1be")
-    BOT_TOKEN = environ.get("BOT_TOKEN", "70955...") 
+    API_ID = environ.get("19024658", "577678")
+    API_HASH = environ.get("9c5b1808b35bc2ae8195351ece057576", "d2c6e01uuiuiouioiuiou0fc6d7a1be")
+    BOT_TOKEN = environ.get("7111533028:AAG5SUFUpSP70l7bJqmDnRNzl-W8DfYR6c8", "70955...") 
     BOT_SESSION = environ.get("BOT_SESSION", "bot") 
     DATABASE_URI = environ.get("DATABASE", "mongodb+srv://chhjgjkkjhkjhkjh@cluster0.xowzpr4.mongodb.net/")
     DATABASE_NAME = environ.get("DATABASE_NAME", "forward-bot")
